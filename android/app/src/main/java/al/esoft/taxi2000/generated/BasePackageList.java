9ed0d3ef2f7338f7cf1a04a3cd0c1ce57f91dc83
Eledi Dyrkaj <edyrkaj@gmail.com>
2020-08-05 13:12:10 +0200

    minor fixes for android packages

--- android/app/src/main/java/al/esoft/taxi2000/generated/BasePackageList.java
+++ android/app/src/main/java/al/esoft/taxi2000/generated/BasePackageList.java
@@ -8,6 +8,7 @@ public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
         new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.device.DevicePackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
@@ -18,6 +19,7 @@ public class BasePackageList {
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
+        new expo.modules.taskManager.TaskManagerPackage(),
         new expo.modules.updates.UpdatesPackage()
     );
   }
